4bc050de3d601e0f27382f566e4b2338f7b621be
aaron yao <vigorpro@gmail.com>
2024-08-28 16:46:41 +0800

    update

--- test.py
+++ test.py
@@ -1,19 +1,34 @@
-
+import torchaudio
 from audiocraft.models import MusicGen
 from audiocraft.data.audio_utils import convert_audio
 from audiocraft.data.audio import audio_write
 from tempfile import NamedTemporaryFile
 
-sample_rate = 32000
-MODEL = MusicGen.get_pretrained('facebook/musicgen-small', device='cpu')
+SAMPLE_RATE = 32000
+
+# 读取 WAV 文件
+wav_path = 'get-lucky-cuts0-14.837551020408164.wav'
+waveform, sr = torchaudio.load(wav_path)
+
+# 如果采样率不匹配，需要进行重采样
+if sr != SAMPLE_RATE:
+    resampler = torchaudio.transforms.Resample(orig_freq=sr, new_freq=SAMPLE_RATE)
+    waveform = resampler(waveform)
+
+MODEL = MusicGen.get_pretrained('facebook/musicgen-small')
 MODEL.set_generation_params(duration=20) 
 
-outputs = MODEL.generate(['disco progressive funk with groovy rhythm and spontaneous ad-libs'], 
-                          progress=True)
+# outputs = MODEL.generate(['disco progressive funk with groovy rhythm and spontaneous ad-libs'], 
+#                           progress=True)
+descriptions = ['disco progressive funk with groovy rhythm and spontaneous ad-libs']
+generated_outputs = MODEL.generate_continuation(prompt=waveform, 
+                                              prompt_sample_rate=SAMPLE_RATE,
+                                              descriptions=descriptions,
+                                              progress=True)
 
-#outputs = convert_audio(outputs, sample_rate, sample_rate, 1)
+#outputs = convert_audio(outputs, SAMPLE_RATE, SAMPLE_RATE, 1)
 
-for output in outputs:
+for output in generated_outputs:
   with NamedTemporaryFile("wb", suffix=".wav", delete=False) as file:
       audio_write(
           file.name, output, 32000, strategy="loudness",
