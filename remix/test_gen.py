7b13ebef71bb1cbe53af5d2776a0fd3a1e139308
aaronyao <vigorpro@gmail.com>
2024-10-02 17:42:21 +0800

    update

--- remix/test_gen.py
+++ remix/test_gen.py
@@ -1,7 +1,31 @@
 import os
+import math
+import torch
 import torchaudio
 from audiocraft.models import MusicGen
 from audiocraft.data.audio import audio_write
+import librosa
+import noisereduce as nr
+
+
+def denoise_my_humming():
+    audio_file = 'my_humming.wav'
+    humming_wav, sr = torchaudio.load(audio_file)
+    noise_length = math.floor(sr * 1.1) # 1 秒的噪声长度
+
+    # 选择音频中的一个静音片段作为噪声的估计
+    noise_sample = humming_wav[0:noise_length] 
+
+    # 使用noisereduce进行降噪
+    denoised_wav = nr.reduce_noise(y=humming_wav.numpy(), sr=sr, y_noise=noise_sample)
+
+    # 保存降噪后的音频
+    denoised_output_file = 'my_humming_denoised.wav'
+    audio_write(
+        denoised_output_file,  torch.from_numpy(denoised_wav), sr, strategy="loudness",
+        loudness_headroom_db=16, loudness_compressor=True, add_suffix=False)
+
+    print(f"Denoised audio saved as {denoised_output_file}")
 
 def test_generate():
     descriptions = ['disco progressive funk with groovy rhythm and spontaneous ad-libs']
@@ -12,7 +36,7 @@ def test_generate():
 
 def test_generate_continuation():
     # 读取 WAV 文件
-    sample_wav_path = 'get-lucky-cuts0-14.837551020408164.wav'
+    sample_wav_path = 'my_humming_denoised.wav'
     sample_wav, sr = torchaudio.load(sample_wav_path)
 
     # 如果采样率不匹配，需要进行重采样
@@ -20,7 +44,7 @@ def test_generate_continuation():
         resampler = torchaudio.transforms.Resample(orig_freq=sr, new_freq=SAMPLE_RATE)
         sample_wav = resampler(sample_wav)
 
-    descriptions = ['disco progressive funk with groovy rhythm and spontaneous ad-libs']
+    descriptions = ['house,EDM']
     MODEL.set_generation_params(duration=60) 
     generated_outputs = MODEL.generate_continuation(prompt=sample_wav, 
                                                 prompt_sample_rate=SAMPLE_RATE,
@@ -31,7 +55,7 @@ def test_generate_continuation():
 
 def test_generate_with_chroma():
     # 读取 WAV 文件
-    melody_wav_path = 'my_humming.wav'
+    melody_wav_path = 'my_humming_denoised.wav'
     melody_wav, sr = torchaudio.load(melody_wav_path)
 
     # 如果采样率不匹配，需要进行重采样
@@ -105,13 +129,16 @@ def plot_chroma():
     plt.show()
 
 if __name__ == '__main__':
+    # denoise_my_humming()
+    
     SAMPLE_RATE = 32000
-    # MODEL = MusicGen.get_pretrained('facebook/musicgen-small')
+    MODEL = MusicGen.get_pretrained('facebook/musicgen-small')
     # MODEL = MusicGen.get_pretrained('facebook/musicgen-large')
-    # test_generate()
+    # MODEL = MAGNeT.get_pretrained('facebook/magnet-medium-30secs')
+    test_generate()
     # test_generate_continuation()
 
-    MODEL = MusicGen.get_pretrained('facebook/musicgen-melody')
-    test_generate_with_chroma()
+    # MODEL = MusicGen.get_pretrained('facebook/musicgen-melody')
+    # test_generate_with_chroma()
 
     # plot_chroma()
